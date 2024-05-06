ef4d1713bc8a17db97e21be2f53a9d68e2c3cb01
hikaruna <169936+hikaruna@users.noreply.github.com>
2024-05-07 05:50:05 +0900

    fix ページ遷移時に遷移前のものが残る問題

--- previewTweet.js
+++ previewTweet.js
@@ -1,6 +1,13 @@
 import 'https://platform.twitter.com/widgets.js';
 
+/** @type {Element[]} */
+const cleanObjects = [];
+
 const previewTweet = () => {
+  for(const i of cleanObjects) {
+    i.remove()
+  }
+
   const twitterLinks = [...document.querySelectorAll('.text .link')].filter(a => a.text.match(/ https:\/\/(?:x|twitter)\.com\/[^/]+\/status\/([0-9]+) /));
   for (const twitterLink of twitterLinks) {
     const href = twitterLink.getAttribute('href');
@@ -10,6 +17,7 @@ const previewTweet = () => {
     if (tweetId == null) { throw 'tweetId is null'; }
     const div = document.createElement('div');
     twitterLink.closest('div.line')?.insertAdjacentElement('afterend', div);
+    cleanObjects.push(div);
     window.twttr.widgets.createTweet(tweetId, div);
   }
 }
